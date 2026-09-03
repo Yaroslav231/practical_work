5ffb20f3f6907e99f5bd3e75b7ba35fc81a2b573
Pirate <a90179742@gmail.com>
2026-09-03 16:30:44 -0700

    task04 results

--- src/hackerrank/task04.rs
+++ src/hackerrank/task04.rs
@@ -1,1 +1,54 @@
 // https://www.hackerrank.com/challenges/grading/problem
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+fn grading_students(grades: &[i32]) -> Vec<i32> {
+    let mut output: Vec<i32> = Vec::with_capacity(grades.len());
+    for &grade in grades {
+        let rounded = ((grade + 4) / 5) * 5;
+        let out_grade = if grade >= 38 && rounded - grade < 3 {rounded} else {grade};
+        output.push(out_grade)
+    }
+    output
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create("output.txt").unwrap();
+
+    let grades_count = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let mut grades: Vec<i32> = Vec::with_capacity(grades_count as usize);
+
+    for _ in 0..grades_count {
+        let grades_item = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+        grades.push(grades_item);
+    }
+
+    let result = grading_students(&grades);
+
+    for i in 0..result.len() {
+        write!(&mut fptr, "{}", result[i]).ok();
+
+        if i != result.len() - 1 {
+            writeln!(&mut fptr).ok();
+        }
+    }
+
+    writeln!(&mut fptr).ok();
+}
+
+#[test]
+fn test() {
+    main()
+}
+
