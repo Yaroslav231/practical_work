d9698d7e9e149651b3faad36cf4fa7d544ebcfa6
Pirate <a90179742@gmail.com>
2026-09-03 16:41:20 -0700

    task05 results

--- src/hackerrank/task05.rs
+++ src/hackerrank/task05.rs
@@ -1,1 +1,75 @@
 // https://www.hackerrank.com/challenges/apple-and-orange/problem
+use std::io::{self, BufRead};
+
+/*
+ * Complete the 'countApplesAndOranges' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER s
+ *  2. INTEGER t
+ *  3. INTEGER a
+ *  4. INTEGER b
+ *  5. INTEGER_ARRAY apples
+ *  6. INTEGER_ARRAY oranges
+ */
+
+fn count_apples_and_oranges(s: i32, t: i32, a: i32, b: i32, apples: &[i32], oranges: &[i32]) {
+    let mut apples_count = 0;
+    let mut oranges_count = 0;
+
+    for i in 0..apples.len() {
+        let apple_position = apples[i] + a;
+        if apple_position >= s && apple_position <= t {apples_count += 1}
+    }
+
+    for i in 0..oranges.len() {
+        let orange_position = oranges[i] + b;
+        if orange_position >= s && orange_position <= t {oranges_count += 1}
+    }
+
+    println!("{}\n{}", apples_count, oranges_count);
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let first_multiple_input: Vec<String> = stdin_iterator.next().unwrap().unwrap()
+        .split(' ')
+        .map(|s| s.to_string())
+        .collect();
+
+    let s = first_multiple_input[0].trim().parse::<i32>().unwrap();
+
+    let t = first_multiple_input[1].trim().parse::<i32>().unwrap();
+
+    let second_multiple_input: Vec<String> = stdin_iterator.next().unwrap().unwrap()
+        .split(' ')
+        .map(|s| s.to_string())
+        .collect();
+
+    let a = second_multiple_input[0].trim().parse::<i32>().unwrap();
+
+    let b = second_multiple_input[1].trim().parse::<i32>().unwrap();
+
+    stdin_iterator.next();
+
+    let apples: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let oranges: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    count_apples_and_oranges(s, t, a, b, &apples, &oranges);
+}
+
+#[test]
+fn test() {
+    main()
+}
